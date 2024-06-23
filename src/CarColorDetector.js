de015f3156f2369ab4eb9651fb76afdb78c235f9
Affan <affan@Hiras-MacBook-Air.local>
2024-06-23 19:06:26 +0500

    added uplaoder min functionality

--- src/CarColorDetector.js
+++ src/CarColorDetector.js
@@ -1,56 +1,63 @@
 import React, { useState, useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+import upload_cloud from "./images/upload_cloud.png";
 
 const CarColorDetector = () => {
   const [files, setFiles] = useState([]);
+  const [dragActive, setDragActive] = useState(false);
 
-  const onDrop = useCallback(
-    (acceptedFiles) => {
-      const filesWithProgress = acceptedFiles.map((file) => ({
-        file,
-        progress: 100, // Mock progress for demo purposes
-      }));
-      setFiles([...files, ...filesWithProgress]);
-    },
-    [files]
-  );
+  const onDrop = useCallback((acceptedFiles) => {
+    setFiles(acceptedFiles);
+  }, []);
 
-  const handleRemove = (fileName) => {
-    setFiles(files.filter((file) => file.file.name !== fileName));
-  };
-
-  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    onDragEnter: () => setDragActive(true),
+    onDragLeave: () => setDragActive(false),
+  });
 
   return (
-    <div className="flex justify-center items-center h-screen bg-blue-900">
-      <div className="flex w-3/4 max-w-4xl shadow-xl rounded-lg overflow-hidden bg-white">
-        <div className="flex-1 p-8 border-r border-gray-200 flex flex-col items-center justify-center">
-          <h2 className="text-2xl font-bold mb-6 text-gray-700">File Upload</h2>
-          <div
-            {...getRootProps()}
-            className="w-full h-64 border-2 border-dashed border-gray-300 rounded-lg flex flex-col items-center justify-center bg-gray-50 cursor-pointer"
-          >
-            <input {...getInputProps()} />
-            <svg
-              className="w-12 h-12 text-gray-400 mb-4"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+    <div>
+      <div className="flex justify-center items-center h-screen bg-[#00091D] bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:30px_30px]">
+        <div className="flex w-9/12 md:w-7/12 lg:w-1/2 max-w-4xl shadow-xl rounded-3xl overflow-hidden bg-white ">
+          <div className="flex-1 p-8 border-r border-gray-200 flex flex-col items-center justify-center">
+            <h2 className="text-2xl font-bold mb-6 text-[#4C4CBD]">
+              File Upload
+            </h2>
+            <div
+              {...getRootProps()}
+              className={`w-full h-71 border-2 border-dashed border-[#4C4CBD] rounded-2xl flex flex-col items-center justify-center bg-gray-50 cursor-pointer ${
+                dragActive? "bg-blue-100" : ""
+              }`}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M3 15a4 4 0 014-4h.586l1.707-1.707a1 1 0 011.414 0L12 11.586V5a1 1 0 011-1h1m4 0h2a2 2 0 012 2v4a1 1 0 01-1 1h-4m-4 4v6m-4-6v6m-4 0v6m-4-6v6m-2-6h2a2 2 0 002-2v-6m0-4h2a2 2 0 012 2v6m0 4h6m-4-4h6"
-              ></path>
-            </svg>
-            <p className="text-gray-500">Drag and Drop Files</p>
-            <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-              Browse
-            </button>
+              <input {...getInputProps()} />
+              <img
+                src={upload_cloud}
+                className="h-[100px] w-[135px] mt-4"
+              ></img>
+              <p className="text-[#4C4CBD] font-semibold my-4">
+                Drag and Drop Files
+              </p>
+              <p className="text-gray-500 mt-3">------ or ------</p>
+              <button className="mt-2 mb-4 bg-[#4C4CBD] px-6 py-1 text-white rounded-full hover:bg-[#383894]">
+                Browse
+              </button>
+              {/* {dragActive && (
+                <p className="text-lg text-blue-500">Drop the files here...</p>
+              )} */}
+            </div>
           </div>
         </div>
+      </div>
+      <div>
+        {files.map((file) => (
+          <div
+            key={file.name}
+            className="flex justify-between items-center mb-4 p-2 bg-gray-50 rounded-lg shadow-sm"
+          >
+            <span className="text-lg">{file.name}</span>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/Cards.js
+++ src/Cards.js
@@ -6,12 +6,12 @@ import people_card from "./images/people_card.jpeg";
 
 function Card({ text, image }) {
   return (
-    <div class="max-w-sm text-white bg-[#1e3b78] border transition duration-300 ease-in-out border-[#1e3b78] hover:scale-110 rounded-lg w-72 h-86 shadow-gray-800 shadow-xl">
+    <div class="max-w-sm text-white bg-[#1e3b78] border transition duration-300 ease-in-out border-[#1e3b78] hover:scale-110 rounded-lg w-72 h-86 my-4 mx-2 md:mx-4 lg:mx-8 shadow-gray-800 shadow-xl">
       {/* bg-[#12203f]*/}
       <a href="#">
         <img class="rounded-t-lg" src={image} alt="" />
         <div class="p-7 text-center">
-          <h5 class="text-2xl font-bold tracking-tight">
+          <h5 class="text-lg md:text-xl lg:text-2xl font-bold tracking-tight">
             {text}
           </h5>
         </div>
@@ -23,9 +23,9 @@ function Card({ text, image }) {
 function Cards() {
   return (
     <div className="bg-[#00091D] ">
-      <div className="px-28 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:30px_30px] pt-36 pb-36">
+      <div className="bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:30px_30px] pt-36 pb-36">
         {/* bg-gradient-to-r from-gray-950 via-slate-900 to-sky-950 */}
-        <div className="flex justify-around">
+        <div className="flex flex-col sm:flex-row justify-center items-center">{/* justify-around what items and justify do?*/}
           <Card text={"Detect Malaria"} image={malaria_card}/>
           <Card text={"Detect Car Color"} image={car_card}/>
           <Card text={"Count Humans"} image={people_card}/>
