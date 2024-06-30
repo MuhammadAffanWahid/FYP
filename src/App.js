acad792e26ac9226cd59dc5845a5219c30245e4e
Affan <affan@Hiras-MacBook-Air.local>
2024-06-30 09:54:20 +0500

    fronend minimalistic

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
+// import React from "react";
+// // import { BrowserRouter, Route, Routes } from "react-router-dom";july 2023
+// import Home from "./Home";
+// import Navbar from "./Navbar";
+// import CarColorDetector from "./CarColorDetector";
+
+// function App() {
+//   return (
+//     // <BrowserRouter>
+//     <div>
+//       <Navbar />
+//       <div>
+//       <Home />
+//       <CarColorDetector/>
+//         {/* <Routes>
+//             <Route path="/home" element={<Home />} />
+//             <Route path="/car-color-detection" element={<Home />} />
+//             <Route path="/human-counting" element={<Home />} />
+//             <Route path="/malaria-detection" element={<Home />} />
+//           </Routes> */}
+//       </div>
+//     </div>
+//     // </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+
 import React from "react";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";july 2023
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Navbar from "./Navbar";
 import CarColorDetector from "./CarColorDetector";
 
 function App() {
   return (
-    // <BrowserRouter>
-    <div>
-      <Navbar />
+    <BrowserRouter>
       <div>
-      <Home />
-      <CarColorDetector/>
-        {/* <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/car-color-detection" element={<Home />} />
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/car-color-detection" element={<CarColorDetector />} />
             <Route path="/human-counting" element={<Home />} />
             <Route path="/malaria-detection" element={<Home />} />
-          </Routes> */}
+          </Routes>
+        </div>
       </div>
-    </div>
-    // </BrowserRouter>
+    </BrowserRouter>
   );
 }
-
 export default App;
--- src/Cards.js
+++ src/Cards.js
@@ -2,13 +2,13 @@ import React from "react";
 import malaria_card from "./images/malaria_card.jpg";
 import car_card from "./images/car_card.jpg";
 import people_card from "./images/people_card.jpeg";
+// import { Link } from "react-router-dom";
 
-
-function Card({ text, image }) {
+function Card({ text, image, redirect }) {
   return (
     <div class="max-w-sm text-white bg-[#1e3b78] border transition duration-300 ease-in-out border-[#1e3b78] hover:scale-110 rounded-lg w-72 h-86 my-4 mx-2 md:mx-4 lg:mx-8 shadow-gray-800 shadow-xl">
       {/* bg-[#12203f]*/}
-      <a href="#">
+      <a href={redirect}>
         <img class="rounded-t-lg" src={image} alt="" />
         <div class="p-7 text-center">
           <h5 class="text-lg md:text-xl lg:text-2xl font-bold tracking-tight">
@@ -25,11 +25,15 @@ function Cards() {
     <div className="bg-[#00091D] ">
       <div className="bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:30px_30px] pt-36 pb-36">
         {/* bg-gradient-to-r from-gray-950 via-slate-900 to-sky-950 */}
-        <div className="flex flex-col sm:flex-row justify-center items-center">{/* justify-around what items and justify do?*/}
-          <Card text={"Detect Malaria"} image={malaria_card}/>
-          <Card text={"Detect Car Color"} image={car_card}/>
-          <Card text={"Count Humans"} image={people_card}/>
-
+        <div className="flex flex-col sm:flex-row justify-center items-center">
+          {/* justify-around what items and justify do?*/}
+          <Card text={"Detect Malaria"} image={malaria_card} redirect={"/"} />
+          <Card
+            text={"Detect Car Color"}
+            image={car_card}
+            redirect={"/car-color-detection"}
+          />
+          <Card text={"Count Humans"} image={people_card} redirect={"/"} />
         </div>
       </div>
     </div>
